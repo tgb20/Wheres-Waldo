3981b70d456b05ac1ae710d8d5607c9663b13d1f
Andrew Pasquale <andrew@elytra.net>
2020-01-16 22:12:54 -0500

    Add comments.

--- waldo.py
+++ waldo.py
@@ -1,15 +1,30 @@
 import cv2
-import numpy as np
 from matplotlib import pyplot as plt
-img_rgb = cv2.imread('waldo_war.jpg')
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+# Looking for this image
 template = cv2.imread('waldo.jpg',0)
-w, h = template.shape[::-1]
+
+# in this image 
+img_rgb = cv2.imread('waldo_feast.jpg')
+
+# Convert it to grayscale
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+# Compare the template to the image
 res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+
+# Get the location where the template best matches the image
 min_score, max_score, (min_x, min_y), (max_x, max_y) = cv2.minMaxLoc(res)
+
+# Get the coordinates of the best match
+width, height = template.shape[::-1]
 corner_topL = (max_x, max_y)
-corner_botR = (corner_topL[0]+template.shape[1], corner_topL[1]+template.shape[0])
-print (corner_topL, corner_botR)
+corner_botR = (corner_topL[0]+width, corner_topL[1]+height)
+print(corner_topL, corner_botR)
+
+# Draw a box on the image 
 cv2.rectangle(img_rgb, corner_topL, corner_botR, (0,255,0), 10)
+
+# Make a new image with the box on it
 cv2.imwrite('found_waldo.png',img_rgb) # Located Waldo
-plt.imsave('map_waldo.png',res) # Map of where the template matcher found "Waldo"ß
+plt.imsave('map_waldo.png',res) # Map of where the template matcher found "Waldo"
